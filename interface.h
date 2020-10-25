fb3336ebdd9e636e50c38e3e0a46982e3665cab6
joaomng <73210841+joaomng@users.noreply.github.com>
2020-10-25 15:06:16 -0300

    Update interface.h

--- interface.h
+++ interface.h
@@ -1,6 +1,12 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "game.h"
 
 void clear(void);
 void printInterface(int matrix[SIZE][SIZE]);
+
+
+#endif
